b35e09b4fcc694ec603d2afee1d1bbc562f95047
harinithak <127740017+harinithak@users.noreply.github.com>
2025-02-05 11:41:38 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-    echo hi123
-    sh 'chmod +x build.sh'
-    sh './build.sh'
-    docker login -u harinitha -p 21-Mar-05
-    docker tag test harinitha/devops_jenkins
-    docker push harinitha/devops_jenkins
-    
